013fdca8e88439e9c6b51dca70029b469252da06
himchan94 <himchan9494@gmail.com>
2022-02-28 15:33:31 +0900

    feat: Changed folder location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { ReportMain } from "./constructor";
+import { ReportMain } from "./constructors";
 
 const App = () => {
   return (
