ea5c02354ef03d10e4eeff356bad1a876f7d7187
hyebinjo <hyebinjo91@gmail.com>
2022-03-01 16:31:54 +0900

    feat: Weave the structure.

--- src/constructors/index.js
+++ src/constructors/index.js
@@ -1,1 +1,3 @@
 export { default as ReportMain } from "./ReportMain";
+export { default as Nav } from "./Nav";
+export { default as Header } from "./Header";
--- src/pages/MonthlyReport.jsx
+++ src/pages/MonthlyReport.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MonthlyReport = () => {
-  return <div></div>;
+  return <div>REPORTMAIN</div>;
 };
 
 export default MonthlyReport;
