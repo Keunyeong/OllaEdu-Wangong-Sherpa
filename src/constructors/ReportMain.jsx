9fd6cf07fb2a82203f84435063fdbf09965efda0
권은서 <kwonenseo@gmail.com>
2022-03-03 16:12:55 +0900

    Create path route to notice

--- src/constructors/ReportMain.jsx
+++ src/constructors/ReportMain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MonthlyReport from "../pages/MonthlyReport";
+import Notice from "../pages/notice/Notice";
 
 const ReportMain = () => {
   return (
     <Routes>
+      <Route path='/notice' element={<Notice />} />
       <Route path='/' element={<MonthlyReport />} />
     </Routes>
   );
