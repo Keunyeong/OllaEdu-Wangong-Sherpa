1aa0edc5702e39eee3055257fcda0e126faa0d16
himchan94 <himchan9494@gmail.com>
2022-03-08 03:19:21 +0900

    feat: Applied contextAPI globally

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 import { ReportMain, Nav, Header } from "../constructors";
-import { ContextProvider } from "../context/Context";
 
 const Main = () => {
   return (
     <Page>
       <Nav depart="police" />
 
-      <ContextProvider>
-        <ReportMain />
-      </ContextProvider>
+      <ReportMain />
     </Page>
   );
 };
@@ -17,7 +14,6 @@ const Main = () => {
 const Page = styled.div`
   display: flex;
   flex-direction: column;
-
   width: 100%;
   height: 100%;
 `;
