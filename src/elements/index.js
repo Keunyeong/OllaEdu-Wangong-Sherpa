e195790ba24508eb9bbb0c7757c15d061d25f756
himchan94 <himchan9494@gmail.com>
2022-03-25 23:56:53 +0900

    feat: Applied new path

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,8 +1,8 @@
 export { default as Card } from "./Card";
-export { default as IrregularPieGraph } from "./IrregularPieGraph";
-export { default as MonthlyGraph } from "./MonthlyGraph";
-export { default as TotalGraph } from "./TotalGraph";
-export { default as GrowthGraph } from "./GrowthGraph";
+export { default as IrregularPieGraph } from "./graph/IrregularPieGraph";
+export { default as MonthlyGraph } from "./graph/MonthlyGraph";
+export { default as TotalGraph } from "./graph/TotalGraph";
+export { default as GrowthGraph } from "./graph/GrowthGraph";
 export { default as ProgressBar } from "./ProgressBar.jsx";
 export { default as Table } from "./Table";
 export { default as SkeletonCircle } from "./skeleton/SkeletonCircle";
